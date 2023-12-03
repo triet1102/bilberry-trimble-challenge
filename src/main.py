e00904f9bf5cba8cfad91564751f61397c4b9357
triet1102 <t.triet.1102@gmail.com>
2023-12-03 16:26:20 +0100

    change to BCELoss + update requirements

--- src/main.py
+++ src/main.py
@@ -1,104 +1,115 @@
 import timm
 import torch
-from torchmetrics.classification import MulticlassAccuracy
+from torchmetrics.classification import BinaryAccuracy
 from src.data_preprocessing.data_module import FieldRoadDatasetKFold
 from tqdm import tqdm
-from torch.utils.tensorboard import SummaryWriter
+from dataclasses import dataclass
 import wandb
 
-writer = SummaryWriter()
+torch.manual_seed(42)
 
-logger = wandb.init(
-    project="image_classification",
-    config={
-        "lr": 1e-2,
-        "nb_folds": 3,
-        "nb_epochs": 5,
-        "optimizer": "AdamW",
-        "freeze_backbone": True,
-    },
+
+@dataclass
+class Config:
+    lr: float
+    nb_folds: int
+    nb_epochs: int
+    optimizer: torch.optim.Optimizer
+    batch_size: int
+    freeze_backbone: bool
+
+
+config = Config(
+    lr=1e-5,
+    nb_folds=3,
+    nb_epochs=10,
+    optimizer=torch.optim.AdamW,
+    batch_size=16,
+    freeze_backbone=True,
 )
 
 model = timm.create_model(
     "convnextv2_tiny.fcmae",
     pretrained=True,
-    num_classes=2,  # remove classifier nn.Linear
+    num_classes=1,  # remove classifier nn.Linear
 )
 for param in model.parameters():
     param.requires_grad = False
 for param in model.head.parameters():
     param.requires_grad = True
 pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
-print(pytorch_total_params)
-nb_epochs = 5
+print(f"Total trainable params: {pytorch_total_params}")
+nb_epochs = 10
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
-loss_function = torch.nn.CrossEntropyLoss()
-accuracy_function = MulticlassAccuracy(num_classes=2)
-softmax = torch.nn.Softmax(dim=-1)
+optimizer = config.optimizer(model.parameters(), lr=config.lr)
+loss_function = torch.nn.BCEWithLogitsLoss()
+accuracy_function = BinaryAccuracy()
 
 # get model specific transforms (normalization, resize)
 data_config = timm.data.resolve_model_data_config(model)
 transforms = timm.data.create_transform(**data_config, is_training=True)
 
-dataset_k_fold = FieldRoadDatasetKFold(transforms=transforms, nb_folds=3, batch_size=16)
+dataset_k_fold = FieldRoadDatasetKFold(
+    transforms=transforms, nb_folds=config.nb_folds, batch_size=config.batch_size
+)
 dataset_k_fold.setup()
 
+logger = wandb.init(project="image_classification", config=config.__dict__)
+
+# define custom step for wandb logger
+logger.define_metric("custom_step")
+for idx in range(dataset_k_fold.nb_folds):
+    logger.define_metric(f"fold_{idx}/train/loss", step_metric="custom_step")
+    logger.define_metric(f"fold_{idx}/train/acc", step_metric="custom_step")
+    logger.define_metric(f"fold_{idx}/val/loss", step_metric="custom_step")
+    logger.define_metric(f"fold_{idx}/val/acc", step_metric="custom_step")
+
 for idx in tqdm(range(dataset_k_fold.nb_folds), total=dataset_k_fold.nb_folds):
-    for epoch in tqdm(range(nb_epochs), total=nb_epochs):
+    global_step = 0
+    for epoch in tqdm(range(config.nb_epochs), total=config.nb_epochs):
         epoch_loss = 0
         train_loader = dataset_k_fold.train_dataloader(idx)
         val_loader = dataset_k_fold.val_dataloader(idx)
 
         # train
         for batch, (data, label) in enumerate(train_loader):
-            output = model(data)
+            output = model(data).squeeze()
 
-            loss = loss_function(output, label)
+            loss = loss_function(output, label.float())
             epoch_loss += loss.item()
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             # log train loss, train accuracy
-            pred = torch.argmax(softmax(output), dim=-1)
-            accuracy = accuracy_function(pred, label)
-            writer.add_scalar(
-                f"fold_{idx}_epoch_{epoch}_train_loss", loss.item(), batch
-            )
-            writer.add_scalar(
-                f"fold_{idx}_epoch_{epoch}_train_acc", accuracy.item(), batch
-            )
+            accuracy = accuracy_function(output, label)
 
             logger.log(
                 {
-                    f"train/loss/fold_{idx}/epoch_{epoch}": loss.item(),
-                    f"train/acc/fold_{idx}_epoch_{epoch}": accuracy.item(),
+                    f"fold_{idx}/train/loss": loss.item(),
+                    f"fold_{idx}/train/acc": accuracy.item(),
+                    "custom_step": global_step,
                 }
             )
+
+            global_step += 1
 
         # validation
         if epoch % 1 == 0:
             model.eval()
             for batch, (data, label) in enumerate(val_loader):
                 with torch.no_grad():
-                    output = model(data)
-                    loss = loss_function(output, label)
+                    output = model(data).squeeze()
+                    loss = loss_function(output, label.float())
 
                 # log train loss, train accuracy
-                pred = torch.argmax(softmax(output), dim=-1)
-                accuracy = accuracy_function(pred, label)
-                writer.add_scalar(
-                    f"fold_{idx}_epoch_{epoch}_val_loss", loss.item(), batch
-                )
-                writer.add_scalar(
-                    f"fold_{idx}_epoch_{epoch}_val_acc", accuracy.item(), batch
-                )
+                accuracy = accuracy_function(output, label)
 
                 logger.log(
                     {
-                        f"val/loss/fold_{idx}/epoch_{epoch}": loss.item(),
-                        f"val/acc/fold_{idx}_epoch_{epoch}": accuracy.item(),
+                        f"fold_{idx}/val/loss": loss.item(),
+                        f"fold_{idx}/val/acc": accuracy.item(),
+                        "custom_step": global_step,
                     }
                 )
 
@@ -106,6 +117,4 @@ for idx in tqdm(range(dataset_k_fold.nb_folds), total=dataset_k_fold.nb_folds):
 
 logger.finish()
 
-writer.flush()
-writer.close()
 print("Finished")
